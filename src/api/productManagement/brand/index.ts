f8476d6b451fc9847d355a62ae61416f66deb3d7
gexiaoyu <2624545321@QQ.com>
2023-12-15 00:51:46 +0800

    chore: fomat

--- src/api/productManagement/brand/index.ts
+++ src/api/productManagement/brand/index.ts
@@ -6,11 +6,13 @@ enum URL {
 }
 
 /**
-* @desc 品牌列表
-* @param {number} page 请求参数
-* @param {number} limit 请求参数
-* 
-* @return {Promise} 请求结果
-*/
+ * @desc 品牌列表
+ * @param {number} page 请求参数
+ * @param {number} limit 请求参数
+ *
+ * @return {Promise} 请求结果
+ */
 export const baseTrademark = (page: number, limit: number): Promise<any> =>
-  request.get<any, BaseTrademarkResponseData>(URL.BASETRADEMARK + `${page}/${limit}`)
+  request.get<any, BaseTrademarkResponseData>(
+    URL.BASETRADEMARK + `${page}/${limit}`,
+  )
